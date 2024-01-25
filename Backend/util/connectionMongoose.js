92c933f21f220d44798ff7a095f21b0086b704a7
omar <yossef>
2024-01-25 16:58:33 +0100

    adding all packeges to backend

--- Backend/util/connectionMongoose.js
+++ Backend/util/connectionMongoose.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 async function connectMongoose() {
   const _pwd = "Oy76811148";
-  const _database = "products";
+  const _database = "MasterTodo";
   const _user = "omar";
   const _cluster = "cluster0.taehtxj.mongodb.net";
   const _uri = `mongodb+srv://${_user}:${_pwd}@${_cluster}/${_database}?retryWrites=true&w=majority`;
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # ToDoMasters
+
 ## Team:
--  Elaheh
--  Omar
--  Dimitar
--  Jonathan
+
+- Elaheh
+- Omar
+- Dimitar
+- Jonathan
+
+## backend:
+
+**npm package**
+- npm init -y
+- npm install express mongoose dotenv 
+- npm install bcryptjs jsonwebtoken 
+- npm npm install express-validator
+- npm install cookie-parser
