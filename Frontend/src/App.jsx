fd0eef8d14c9c79a7210a7388d2e4bd16e4b61d2
omar <yossef>
2024-01-28 13:58:51 +0100

    adding a card component in fronend to test it ,we should connect it with backeend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import LoginPage from "./pages/loginPage.jsx";
+import WorkFlowCard from "./components/WorkFlowCard.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <WorkFlowCard />
     </>
   );
 }
