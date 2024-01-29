85b1176edfb18fbedb1a410226cd71a5a5cfec28
omar <yossef>
2024-01-29 14:07:25 +0100

    new update for form

--- Frontend/src/pages/loginPage.jsx
+++ Frontend/src/pages/loginPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import cookie from "js-cookie";
 import axios from "axios";
-import "./Form.css";
+import "../styles/form.css";
 
 function Form() {
   const [hasToken, setHasToken] = useState(false);
