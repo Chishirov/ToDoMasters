366012b3e25fd6d9b34f2d4adccd01b539a970fd
omar <yossef>
2024-01-29 14:02:20 +0100

    update Form

--- Frontend/src/pages/loginPage.jsx
+++ Frontend/src/pages/loginPage.jsx
@@ -1,115 +1,269 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import cookie from "js-cookie";
 import axios from "axios";
-function loginPage() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [logemail, setlogEmail] = useState("");
-  const [logpassword, setlogPassword] = useState("");
-  const [user, setUser] = useState(null);
+import "./Form.css";
 
-  const handleLogin = async () => {
+function Form() {
+  const [hasToken, setHasToken] = useState(false);
+  const [error, setError] = useState("");
+  const [msg, setMsg] = useState("");
+  const [user, setUser] = useState({});
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const resetMessages = () => {
+    setMsg("");
+    setError("");
+  };
+
+  const setErrorMessages = (error) => {
+    if (error.response) {
+      setError(error.response.data.error);
+    } else {
+      setError(error.message);
+    }
+  };
+
+  const backendApiUrl = "http://localhost:3005";
+  // const backendApiUrl =
+  // process.env.NODE_ENV === "development"
+  // ? "http://localhost:3005"
+  // : "https://myproject233.render.com"
+
+  const handleSignup = async (e) => {
+    console.log("handleSignup ausgefühlt");
+    // const form = e.target;
+    // const email = form.email.value;
+    // const password = form.password.value;
+    e.preventDefault();
+    console.log("email in handleSignup", email);
+    console.log("password in handleSignup", password);
+
+    resetMessages();
+    try {
+      const response = await axios.post(`${backendApiUrl}/signup`, {
+        email,
+        password,
+        name: username,
+      });
+      // const response = await axios.post(`http://localhost:3005/signup`, { email, password, name: username });
+
+      setUser(response.data.user);
+      setMsg("You have successfully registered.");
+      setIsLoggedIn(true);
+    } catch (error) {
+      setErrorMessages(error);
+      console.log("error while signing up:", error);
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // const form = e.target;
+    // const email = form.email.value;
+    // const password = form.password.value;
+    resetMessages();
     try {
       const response = await axios.post(
-        "http://localhost:3005/login",
+        `${backendApiUrl}/login`,
+        { email, password },
+        { withCredentials: true }
+      );
+      setUser(response.data.user);
+      setHasToken(true);
+      setIsLoggedIn(true);
+      setMsg(`You have successfully logged in: ${email}. JWT received.`);
+      console.log("email in handleLogin", email);
+      console.log("password in handleLogin", password);
+    } catch (error) {
+      setErrorMessages(error);
+      console.log("error while logging in:", error);
+    }
+  };
+
+  const handleLogout = async () => {
+    // e.preventDefault();
+    resetMessages();
+    try {
+      const response = await axios.post(
+        // `http://localhost:3005/logout`,
+
+        `${backendApiUrl}/logout`,
+        {},
+        { withCredentials: true }
+      );
+      setMsg("You have successfully logged out.", response.data);
+      console.log("Logout successful:", response.data);
+      setHasToken(false);
+      setIsLoggedIn(false);
+      setUser({});
+    } catch (error) {
+      setErrorMessages(error);
+      console.log("error while logging out:", error);
+    }
+  };
+
+  const handleIfUserHasToken = () => {
+    let JWTinfocookie = cookie.get("JWTinfo");
+    if (!JWTinfocookie) return;
+
+    JWTinfocookie = JWTinfocookie.replace("j:", "");
+    const cookieValueObj = JSON.parse(JWTinfocookie);
+    console.log("cookieValueObj", cookieValueObj);
+
+    const expirationInMs = new Date(cookieValueObj.expires) - new Date();
+
+    if (expirationInMs <= 0) return;
+
+    setHasToken(true);
+    setUser({ email: cookieValueObj.email });
+    setMsg(`Logged in user: ${cookieValueObj.email}.`);
+  };
+
+  const userInfoHandler = async () => {
+    resetMessages();
+    try {
+      const resp = await axios.get(`${backendApiUrl}/userinfo`, {
+        // const resp = await axios.get(`http://localhost:3005/userinfo`, {
+        withCredentials: true,
+      });
+      console.log("resp.data:", resp.data);
+      setMsg(resp.data);
+    } catch (error) {
+      setErrorMessages(error);
+    }
+  };
+
+  const handlePasswordUpdate = async (e) => {
+    e.preventDefault();
+    resetMessages();
+    try {
+      const response = await axios.post(
+        `${backendApiUrl}/update-password`,
+        formData,
         {
-          email: logemail,
-          password: logpassword,
-        },
-        {
+          // const response = await axios.put(`http://localhost:3005/update-password`, formData, {
           withCredentials: true,
         }
       );
-      console.log("Server Response Data:", response.data);
-      setUser(response.data.user);
+      console.log("Server Response:", response.data);
     } catch (error) {
-      if (error.response) {
-        console.error("Fehlerhafte Serverantwort:", error.response.data);
-      } else if (error.request) {
-        console.error("Keine Antwort vom Server erhalten:", error.request);
-      } else {
-        console.error("Fehler beim Einloggen:", error.message);
-      }
+      setErrorMessages(error);
+      console.log("error while update-password:", error);
     }
   };
+
   useEffect(() => {
-    console.log("user:", user);
-  }, [setUser]);
-  console.log(user);
-  const handleSignup = async () => {
-    try {
-      const response = await fetch("http://localhost:3005/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          password: password,
-        }),
-      });
-      if (response.status === 201) {
-        const data = await response.json();
-        console.log(data.message);
-      } else {
-        console.error("Fehler beim Registrieren:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Fehler beim Registrieren:", error);
+    handleIfUserHasToken();
+    const token = cookie.get("token");
+    if (token) {
+      setHasToken(true);
     }
-  };
+  }, []);
+
   return (
-    <div>
-      {user ? (
+    <div className="container">
+      <div className="form_area">
+        <p className="title sub_title">SIGN UP</p>
+        <h3 style={{ color: "orange" }}>
+          DEBUG-Message: {hasToken ? "User is logged in." : "Not logged in!!!"}
+        </h3>
+        <p className="info">
+          <span style={{ color: "red", fontSize: "0.7rem" }}>{error}</span>{" "}
+          <span>{msg}</span>
+        </p>
         <div>
-          <h1>
-            Willkommen,{" "}
-            <div>{user?.name ?? "Benutzername nicht verfügbar"}</div>!
-            {/* {<p>{user.name} </p>} */}
-          </h1>
-          {/* Hier könntest du die Todo-Logik hinzufügen */}
+          {!hasToken ? (
+            <div className="form_group">
+              <label className="sub_title" htmlFor="username">
+                Username:
+              </label>
+              <input
+                className="form_style"
+                id="username"
+                name="username"
+                type="text"
+                placeholder="Enter your username"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <br />
+              <label className="sub_title" htmlFor="email">
+                Email:
+              </label>
+              <input
+                className="form_style"
+                placeholder="Enter your email"
+                id="email"
+                name="email"
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <br />
+              <label className="sub_title" htmlFor="password">
+                Password:
+              </label>
+              <input
+                className="form_style"
+                placeholder="Enter your password"
+                id="password"
+                name="password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <div className="btn">
+                <button onClick={handleSignup}>Signup</button>
+                <button onClick={handleLogin}>Login</button>
+              </div>
+            </div>
+          ) : (
+            <div className="form_group">
+              <p>Logged in as: {user.email}</p>
+              <button className="btn" onClick={handleLogout}>
+                Logout
+              </button>
+              <label className="sub_title" htmlFor="password">
+                New Password:
+              </label>
+
+              <input
+                className="form_style"
+                placeholder="Enter your password"
+                id="password"
+                name="password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="btn" onClick={handlePasswordUpdate}>
+                Update Password
+              </button>
+              <hr />
+              <button
+                style={{ background: "#F3B95F", margin: "0 auto" }}
+                onClick={userInfoHandler}
+              >
+                Zeige persönliche Daten
+              </button>
+            </div>
+          )}
         </div>
-      ) : (
-        <div>
-          <h1>Login</h1>
-          <input
-            type="email"
-            placeholder="Email"
-            value={logemail}
-            onChange={(e) => setlogEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Passwort"
-            value={logpassword}
-            onChange={(e) => setlogPassword(e.target.value)}
-          />
-          <button onClick={handleLogin}>Login</button>
-          <h1>Registrieren</h1>
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Passwort"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={handleSignup}>Registrieren</button>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
 
-export default loginPage;
+export default Form;
