0aa0fa78bfa0cedf9cdb9a4f76ef1d97732c8fe6
Dimitar Chishirov <dimitarchishirov@gmail.com>
2024-02-01 15:14:44 +0100

    App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,65 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
-import LoginPage from "./pages/loginPage.jsx";
+import Login from "./Login";
+import { useContext, useEffect, useState } from "react";
+import cookie from "js-cookie"; // cookie parser
+
+import WorkFlowPage from "./WorkFlowPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserContext } from "./context/UserContext";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const {
+    hasToken,
+    setHasToken,
+    error,
+    setError,
+    msg,
+    setMsg,
+    user,
+    setUser,
+    userId,
+    setUserId,
+  } = useContext(UserContext);
 
+  const handleIfUserHasToken = () => {
+    console.log("handleIfUserHasToken aufgerufen");
+
+    let JWTinfocookie = cookie.get("JWTinfo");
+
+    console.log("JWTinfo cookie", JWTinfocookie);
+    if (!JWTinfocookie) return;
+
+    JWTinfocookie = JWTinfocookie.replace("j:", "");
+    const cookieValueObj = JSON.parse(JWTinfocookie);
+    console.log("cookieValueObj", cookieValueObj);
+    setUserId(cookieValueObj.user._id);
+
+    const expirationInMs = new Date(cookieValueObj.expires) - new Date();
+    console.log("JWT läuft ab in", expirationInMs / 1000, "Sekunden");
+
+    if (expirationInMs <= 0) return;
+
+    setHasToken(true);
+    setUser(cookieValueObj.user);
+    setMsg(`Eingeloggter User: ${cookieValueObj.email}.`);
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      await handleIfUserHasToken();
+    };
+    fetchData();
+  }, [hasToken]);
   return (
-    <>
-      <LoginPage />
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/workflow" element={<WorkFlowPage />} />
+          {/* Define other routes as needed */}
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
