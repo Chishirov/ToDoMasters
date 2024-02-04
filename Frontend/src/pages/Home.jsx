58d0debeb9bcc0a976f55993c4143847b3cba238
omar <yossef>
2024-02-04 11:23:40 +0100

    adding transfom to the loginbutton in the home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import LoginButton from "../components/LoginButton";
+import "../styles/home.css";
 function Home() {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      {/* <button
+          onClick={() => navigate("/login")}
+          style={{ background: "initial", border: "1px solid" }}
+        >
+          Login
+        </button> */}
+      <div className="home-login-button">
+        <LoginButton />
+      </div>
+
+      <div>
+        <button
+          onClick={() => navigate("/signup")}
+          style={{ background: "initial", border: "1px solid" }}
+        >
+          Sugnup
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
